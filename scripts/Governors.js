0bc9797e64e542cd14e893a0a179474caff034a0
Jake Brooks <jacobdbrooks1998@gmail.com>
2022-02-14 15:46:57 -0600

    governor drop down menu created

--- scripts/Governors.js
+++ scripts/Governors.js
@@ -1,15 +1,35 @@
 // import getGovernors setGovernors()
+import {getGovernors, setGovernor} from "./database.js"
 // import colonies module (Colonies())
+import {Colonies} from "./colonies.js"
 // Define and export a function that 
 // iterates through the list of governors
 // and returns an HTML string for a drop down selection menu
     // condtional that determines if governor is active
     // if inactive, do not display
-
+export const Governors = () => {
+    const governors = getGovernors();
+        let html = '<option value="0">Select governor</option>'
+    
+        for (const governor of governors) {
+            html += `<option value="${governor.id}">${governor.name}</option>`
+        }
+    
+        html += "</select>"
+        return html
+        
+}
 //when governor is selected, 
 // the associated colony's name and resources will appear 
 // // if governor.colonyId === colony ID, display
 
 // Create event listener that listens for change or click event int the governor selection
     //calls a function that displays the governors colony and its resources (Colonies())
-    
+    document.addEventListener(
+        "change",
+        (event) => {
+            if (event.target.id === "governors") {
+                setGovernor(parseInt(event.target.value))  
+            }
+        }
+    )
