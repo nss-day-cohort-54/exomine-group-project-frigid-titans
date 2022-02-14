bbdcb2f5b74667c02500e03f80076fcfdc8062ba
Erich Hils <mrerichhils@gmail.com>
2022-02-14 13:05:55 -0600

    feat: main.js and database created.

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+const mainContainer = document.querySelector(".container")
+
+// renderAllHTML needed for growth of application
+const renderAllHTML = () => {
+    mainContainer.innerHTML = Exomine()
+}
+
+renderAllHTML()
